dcc411b5b109910d1831304ea98e0bf7dc4b7c35
wangxin <kingbsby@163.com>
2022-03-09 00:42:11 +0800

    build deploy

---
+++ precache-manifest.a7a99a9cfb8d6f00626ee81814e60a22.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6f8f057db4bcd4b1f05257d8cea4e99",
+    "url": "/my-copyright/index.html"
+  },
+  {
+    "revision": "58133c1f7d069bf63e63",
+    "url": "/my-copyright/static/css/2.885c3557.chunk.css"
+  },
+  {
+    "revision": "8d398742b41f7b617dcb",
+    "url": "/my-copyright/static/css/main.a3655d9a.chunk.css"
+  },
+  {
+    "revision": "58133c1f7d069bf63e63",
+    "url": "/my-copyright/static/js/2.c8b6a92a.chunk.js"
+  },
+  {
+    "revision": "d119518c1c83abce07fd753ab411defe",
+    "url": "/my-copyright/static/js/2.c8b6a92a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d398742b41f7b617dcb",
+    "url": "/my-copyright/static/js/main.fcca188e.chunk.js"
+  },
+  {
+    "revision": "8e28dcbea1397897b1a7",
+    "url": "/my-copyright/static/js/runtime-main.26b4f9e1.js"
+  }
+]);
